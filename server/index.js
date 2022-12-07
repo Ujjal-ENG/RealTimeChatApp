c17f47aa1e146bf4afa6058c95a4352cd84b6caa
user <ujjalroy7862@gmail.com>
2022-12-08 00:54:59 +0600

    now create message server

--- server/index.js
+++ server/index.js
@@ -1,2 +1,35 @@
-const http = require('http');
-const express = require('express')
+const http = require("http");
+const express = require("express");
+const cors = require("cors");
+const socketIO = require("socket.io");
+
+const app = express();
+const port = 4500 || process.env.PORT;
+
+app.use(cors());
+const server = http.createServer(app);
+
+const io = socketIO(server);
+
+const users = [{}];
+
+app.get("/", (req, res) => {
+  res.send("This is the Home Page");
+});
+
+io.on("connection", (socket) => {
+  console.log("New Connection");
+
+  socket.on("joined", ({ user }) => {
+    users[socket.id] = user;
+    console.log(`${user} has joined`);
+  });
+
+  socket.emit('welcome', { user: 'Admin', message: `Welcome to the chat` })
+  
+  socket.broadcast.emit('userJoined',{user:'Admin',message: `${users.socket.id} has Joined`})
+});
+
+server.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
